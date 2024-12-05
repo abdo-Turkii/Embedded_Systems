29fbe9ebbcef0d1b22c77b8429ee7076f501f349
Turkii <abdelrohman.mohamed88@gmail.com>
2024-12-05 11:56:29 +0000

    Master sent and resive data then print it

---
+++ ads1115_emulation/ads1115.c
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2021 Valentin Milea <valentin.milea@gmail.com>
+ * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <hardware/i2c.h>
+#include <pico/i2c_slave.h>
+#include <pico/stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+static const uint I2C_SLAVE_ADDRESS = 0x055;
+static const uint I2C_BAUDRATE = 100000; // 100 kHz
+#ifdef i2c_default
+// For this example, we run both the master and slave from the same board.
+// You'll need to wire pin GP4 to GP6 (SDA), and pin GP5 to GP7 (SCL).
+static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
+static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
+static const uint I2C_MASTER_SDA_PIN = 18;
+static const uint I2C_MASTER_SCL_PIN = 19;
+
+// The slave implements a 256 byte memory. To write a series of bytes, the master first
+// writes the memory address, followed by the data. The address is automatically incremented
+// for each byte transferred, looping back to 0 upon reaching the end. Reading is done
+// sequentially from the current memory address.
+static uint8_t buffer[4]={56,24,54,44};
+static uint8_t buffer_index = 0  ; 
+static bool buffer_index_written ;
+
+// Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
+// printing to stdio may interfere with interrupt handling.
+static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    switch (event) {
+    case I2C_SLAVE_RECEIVE: // master has written some data
+        if (!buffer_index_written) {
+            // writes always start with the memory address
+            buffer_index = i2c_read_byte_raw(i2c);
+            buffer_index_written = true;
+        } else {
+            // save into memory
+            buffer[buffer_index] = i2c_read_byte_raw(i2c);
+            //buffer_index++;
+        }
+        break;
+    case I2C_SLAVE_REQUEST: // master is requesting data
+        // load from memory
+        i2c_write_byte_raw(i2c, buffer[buffer_index]);
+        //buffer_index++;
+        break;
+    case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+        buffer_index_written = false;
+        break;
+    default:
+        break;
+    }
+}
+
+static void setup_slave() {
+    gpio_init(I2C_SLAVE_SDA_PIN);
+    gpio_set_function(I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SLAVE_SDA_PIN);
+
+    gpio_init(I2C_SLAVE_SCL_PIN);
+    gpio_set_function(I2C_SLAVE_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SLAVE_SCL_PIN);
+
+    i2c_init(i2c0, I2C_BAUDRATE);
+    // configure I2C0 for slave mode
+    i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
+}
+
+static void run_master() {
+    gpio_init(I2C_MASTER_SDA_PIN);
+    gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
+    // pull-ups are already active on slave side, this is just a fail-safe in case the wiring is faulty
+    gpio_pull_up(I2C_MASTER_SDA_PIN);
+
+    gpio_init(I2C_MASTER_SCL_PIN);
+    gpio_set_function(I2C_MASTER_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_MASTER_SCL_PIN);
+
+    i2c_init(i2c1, I2C_BAUDRATE);
+
+    for (buffer_index = 0;; buffer_index = (buffer_index + 1) % 4) {
+    
+        int i =99 ;
+        uint8_t buf[2];
+        buf[0] = buffer_index;
+        buf[1] = i ;
+        i++;
+        // write message at mem_address
+        printf("Write at %d: '%d'\n", buffer_index,buf[1]);
+        int count = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 2 , false);
+        if (count < 0) {
+            puts("Couldn't write to slave, please check your wiring!");
+            return;
+        }
+
+        //seek to mem_address
+        i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, true);
+        //  read
+        buf[1]=0;
+        i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, &buf[1], 1, true);
+        printf("Read  at %d: '%u'\n", buffer_index, buf[1]);
+        puts("");
+        sleep_ms(2000);
+    }
+}
+#endif
+
+int main() {
+    stdio_init_all();
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning i2c / slave_mem_i2c example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+    return 0;
+#else
+int c ;
+scanf("%d",&c);
+    puts("\nI2C slave example");
+
+    setup_slave();
+    run_master();
+#endif
+}
--- keypad/main.c
+++ keypad/main.c
@@ -1,22 +1,12 @@
 #include "keypad.h"
 #include <pico/time.h>
+    int  x = 0 , j = 0 ;
 
-
-int main()
+void main_keypad()
 {
-    stdio_init_all();
-
-    int i=0 ,x = 0 , j = 0 ;
     char pass[6] = {'A','B','7','8','*','#'};
     char key  ; 
-    init_gpio();
-    lcd_initalise();
-    lcd_string("The Password is :");
-    transfer(LCD_CURSORSECONDLINE, LCD_COMMAND);
-    transfer(LCD_CURSORINCREAMENT,LCD_COMMAND);
-    while (true)
-    {
-        key = scan_keypad();
+    key = scan_keypad();
         if (key != '\0')
         {   
             lcd_char('*');
@@ -35,7 +25,7 @@ int main()
         
             while (true)
             {
-                key = scan_keypad();
+                /*key = scan_keypad();
                 if (key != '\0')
                 {
                     transfer(LCD_CLEARDISPLAY, LCD_COMMAND);
@@ -43,7 +33,7 @@ int main()
                     printf("Key pressed: %c\n", key);
                 }
                 sleep_ms(DELAY/2);
-            }
+            }*/
         }
         printf("The value of x = %d\n",x);
         if(x > 5)
@@ -57,6 +47,20 @@ int main()
             transfer(LCD_CURSORSECONDLINE, LCD_COMMAND);
             transfer(LCD_CURSORINCREAMENT,LCD_COMMAND);
         }
-        
+}
+
+int main()
+{
+    stdio_init_all();
+
+    
+    init_gpio();
+    lcd_initalise();
+    lcd_string("The Password is :");
+    transfer(LCD_CURSORSECONDLINE, LCD_COMMAND);
+    transfer(LCD_CURSORINCREAMENT,LCD_COMMAND);
+    while (true)
+    {
+        main_keypad();
     }
 }
