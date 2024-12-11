39dcb9809e9f078556c614867cd731de3f6cbf5e
Turkii <abdelrohman.mohamed88@gmail.com>
2024-12-11 06:49:02 +0000

    config and ADC result

--- ads1115_emulation/ads1115.c
+++ ads1115_emulation/ads1115.c
@@ -1,61 +1,101 @@
-/*
- * Copyright (c) 2021 Valentin Milea <valentin.milea@gmail.com>
- * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
+#include <hardware/gpio.h>
 #include <hardware/i2c.h>
+#include <pico/critical_section.h>
 #include <pico/i2c_slave.h>
 #include <pico/stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include "hardware/adc.h"
 
-static const uint I2C_SLAVE_ADDRESS = 0x055;
-static const uint I2C_BAUDRATE = 100000; // 100 kHz
+#define PIN_BUTTON 24
+#define ADC_26 26
+#define ADC_27 27
+#define ADC_28 28
+#define ADC_29 29
+
+static const uint I2C_SLAVE_ADDRESS = 0x48;
+static const uint I2C_BAUDRATE = 100*1000; // 100 kHz 
+static uint active_adc = 0;
+static uint fsr = 2;
+static double gain ,result ;
+critical_section_t myLock;
+
 #ifdef i2c_default
-// For this example, we run both the master and slave from the same board.
-// You'll need to wire pin GP4 to GP6 (SDA), and pin GP5 to GP7 (SCL).
-static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
-static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
-static const uint I2C_MASTER_SDA_PIN = 18;
-static const uint I2C_MASTER_SCL_PIN = 19;
+// For this program, we emulat ADS1115 by using RP2040.
+// You'll need to wire pin GPIO 18 (SDA), and pin GPIO 19 (SCL).
+static const uint I2C_SLAVE_SDA_PIN = 18; 
+static const uint I2C_SLAVE_SCL_PIN = 19; 
 
 // The slave implements a 256 byte memory. To write a series of bytes, the master first
-// writes the memory address, followed by the data. The address is automatically incremented
-// for each byte transferred, looping back to 0 upon reaching the end. Reading is done
-// sequentially from the current memory address.
-static uint8_t buffer[4]={33,57,49,88};
-static uint8_t buffer_index = 0  ; 
-static bool buffer_index_written ;
+// writes the memory address, followed by the data.
+static uint16_t buffer[4]={56,24,54,44};
+static uint8_t buffer_index = 0  ;   //refar to the number of register
+static bool buffer_index_written ;   // acknowledge bit
+
+double get_adc_read()
+{
+    buffer[0] = adc_read();
+    result = (buffer[0]) / 4095 ;
+    result = result * gain ;
+}
+
+void adc_config()
+{
+    // Initialise the ADC. reset clock for ABC 
+    adc_init();                                 
+    adc_gpio_init(ADC_26);                   // GPIO 26 use as ADC pin
+    adc_gpio_init(ADC_27);                  // GPIO 27 use as ADC pin
+    adc_gpio_init(ADC_28);                 // GPIO 28 use as ADC pin
+    adc_gpio_init(ADC_29);                // GPIO 29 use as ADC pin  
+
+}
+void adc_settings()
+{
+    //Select adc pin input from  pin 0 to 3
+    active_adc = (buffer[0]>> 12) & 3;
+    adc_select_input(active_adc); 
+
+    //Seleect PAG grom FSR = 0.256 to 6.144 V
+    fsr = (buffer[0]>> 8) & 7;
+    if((fsr == 7) ||(fsr == 6) || (fsr == 5) ){gain = 0.256 ;}
+    else if(fsr == 4){gain = 0.512 ;}
+    else if(fsr == 3){gain = 1.024 ;}
+    else if(fsr == 2){gain = 2.048 ;}
+    else if(fsr == 1){gain = 4.055 ;}
+    else if(fsr == 0){gain = 6.144 ;}
+}
 
 // Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
 // printing to stdio may interfere with interrupt handling.
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    //critical zone start
+    critical_section_enter_blocking(&myLock);
+    
     switch (event) {
     case I2C_SLAVE_RECEIVE: // master has written some data
         if (!buffer_index_written) {
             // writes always start with the memory address
-            buffer_index = i2c_read_byte_raw(i2c);
+            buffer_index = i2c_read_byte_raw(i2c); 
             buffer_index_written = true;
         } else {
             // save into memory
             buffer[buffer_index] = i2c_read_byte_raw(i2c);
-            //buffer_index++;
         }
         break;
-    case I2C_SLAVE_REQUEST: // master is requesting data
-        // load from memory
-        i2c_write_byte_raw(i2c, buffer[buffer_index]);
-        //buffer_index++;
+    case I2C_SLAVE_REQUEST: // master is requesting data load from ADC
+        get_adc_read();
+        i2c_write_byte_raw(i2c, result);
         break;
     case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
         buffer_index_written = false;
+        adc_settings();
         break;
     default:
         break;
     }
+    //critical zone end
+    critical_section_exit(&myLock);
 }
 
 static void setup_slave() {
@@ -71,44 +111,28 @@ static void setup_slave() {
     // configure I2C0 for slave mode
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
 }
-
-static void run_master() {
-    gpio_init(I2C_MASTER_SDA_PIN);
-    gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
-    // pull-ups are already active on slave side, this is just a fail-safe in case the wiring is faulty
-    gpio_pull_up(I2C_MASTER_SDA_PIN);
-
-    gpio_init(I2C_MASTER_SCL_PIN);
-    gpio_set_function(I2C_MASTER_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_MASTER_SCL_PIN);
-
-    i2c_init(i2c1, I2C_BAUDRATE);
-
-    for (buffer_index = 0;; buffer_index = (buffer_index + 1) % 4) {
-    
-        uint8_t buf[1];       
-        //read from i2c slave buffer
-        buf[0]=0 ;
-        i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, &buf[0], 1, true);
-        printf("Read  at %d: '%u'\n", buffer_index, buf[0]);
-        puts("");
-        sleep_ms(2000);
-    }
-}
 #endif
 
 int main() {
     stdio_init_all();
+    
+    //initilisation ADC chanels 
+    adc_config();
+    //initilaization of critical section
+    critical_section_init(&myLock);
+
+    //set interrput priority
+    irq_set_priority(I2C0_IRQ,0x00);   //Highest priority of irq
+
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
 #warning i2c / slave_mem_i2c example requires a board with I2C pins
     puts("Default I2C pins were not defined");
     return 0;
 #else
-int c ;
-scanf("%d",&c);
-    puts("\nI2C slave example");
 
+    //operating function 
     setup_slave();
-    run_master();
+    while (true);
+   
 #endif
 }
