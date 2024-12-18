272afb4aa6338a4da97c7bf2ccaea6643c5797f1
Turkii <abdelrohman.mohamed88@gmail.com>
2024-12-18 20:02:50 +0000

    Misson done ,ADS1115 EMULATION.

--- ads1115_emulation/ads1115.c
+++ ads1115_emulation/ads1115.c
@@ -3,15 +3,20 @@
 #include <pico/critical_section.h>
 #include <pico/i2c_slave.h>
 #include <pico/stdlib.h>
+#include <pico/time.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include "hardware/adc.h"
-// For this program, we emulat ADS1115 by using RP2040.
-
-// from section [9.6.3]  Config Register Field Descriptions 
-//table Config Register Field Descriptions
+// For this program, i try to  emulate ADS1115 by using RP2040.
+// from RP2040 i using GPIO(26,27,28,29) as a ADC converter with 12 bits resolution. 
+// ADS1115 have a 16 bits resolution so we convert 12 bits to 16 bits to send data in 16 bits 
+// Datasheet of ADS1115 [https://www.ti.com/lit/ds/symlink/ads1115.pdf].
+// Git hub link [].
+static bool config_flag;
+// From data sheet of ADS115 section [9.6.3] Config Register Field Descriptions 
+// Table(8) Config Register Field Descriptions.
 struct config_register {
     unsigned int comp_que : 2;      //Comparator queue and disable      
     unsigned int comp_lat : 1;     //This bit controls whether the ALERT/RDY pin latches
@@ -30,33 +35,26 @@ struct config_register {
 #define ADC_28 28
 #define ADC_29 29
 
-static uint16_t set_configration =0x8483  ,lo_thresh_reg =0x8000,ho_thresh_reg=0x7FFF, input_signal , response  ;
-static uint8_t add_ptr_reg = 0 , msb , lsb ;   // reg_add refar to the number of register
-uint16_t max_range ;                      // max range for input signal
-static bool ack ;                        // acknowledge bit
 // I2C address and pins
-static const uint I2C_SLAVE_ADDRESS = 0x055;   //address of i2c slave in rp2040
-static const uint I2C_BAUDRATE = 100*1000;    // 100 kHz 
+#define I2C_SLAVE_ADDRESS  0x055   //address of i2c slave in RP2040
+#define I2C_BAUDRATE  400*1000    // 400 kHz for i2c bus datarate transfer
 
+// You'll need to wire pin GPIO 18 (SDA), and pin GPIO 19 (SCL).
+#define I2C_SLAVE_SDA_PIN 18
+#define I2C_SLAVE_SCL_PIN 19 
+
+// From data sheet of ADS115 section [9.6] Register Map for ADS1115  
+// Table (6). Address Pointer Register Field Descriptions
+static uint16_t set_configration =0x8483  ,lo_thresh_reg =0x8000,ho_thresh_reg=0x7FFF  ;
+static uint8_t add_ptr_reg = 0;             // add_ptr_reg refar to the number of register
+static uint8_t msb = 0x84 , lsb = 0x83 ;   // 2 bytes of data come from master i2c bus
+uint16_t max_range ;                      // max range for input signal
+uint16_t input_signal ,response ;                 // variable for signal readed by RP2040 
 //define critical section lock
 critical_section_t myLock;
 
 #ifdef i2c1
-// You'll need to wire pin GPIO 18 (SDA), and pin GPIO 19 (SCL).
-static const uint I2C_SLAVE_SDA_PIN = 18; 
-static const uint I2C_SLAVE_SCL_PIN = 19; 
-
-//Function to get signal and convert it to 15 bits.
-void get_adc_read()
-{
-    input_signal = adc_read();             //get signal from ADC after convertion it to digital
-    if(input_signal >= max_range)         //saturate signal 
-        response = 32767;                //MAX in range 
-    else
-        response = (input_signal * 32767 ) / max_range ;   //Store signal in 15 bits 
-}
-
-//Function to initialize ADC GPIO
+// Function to initialize ADC GPIO
 void adc_config()
 {
     // Initialise the ADC. reset clock for ABC 
@@ -66,6 +64,15 @@ void adc_config()
     adc_gpio_init(ADC_28);                 // GPIO 28 use as ADC pin
     adc_gpio_init(ADC_29);                // GPIO 29 use as ADC pin  
 }
+//Function to get signal and convert it to 15 bits.
+void get_adc_read()
+{
+    input_signal = adc_read();               //get signal from ADC after convertion it to digital
+    if(input_signal >= max_range)           //saturate signal 
+        response = 32767;                //MAX in range . 
+    else
+        response = (input_signal * 32767 ) / max_range ;   //Store signal in 15 bits 
+}
 
 //Function to apply configration 
 void adc_settings()
@@ -73,8 +80,9 @@ void adc_settings()
     //Select adc pin input from  pin 0 to 3 (Gpio from 26 to 29)
     adc_select_input(config.mux % 4); 
 
-    //from section [9.6.3] Config Register
-    //Seleect PGA from FSR = 0.256 to 6.144 V
+    //From datas sheet of ADS1115 insection [9.3.3] Full-Scale Range (FSR) and LSB Size
+    //Table (3). Full-Scale Range and Corresponding LSB Size
+    //Seleect PGA from [FSR = 0.256 to 6.144]V
     if(config.pga >= 5 )    {max_range   = 328     ;}  // get read in range from 0 to 256  mV
     else if(config.pga == 4){max_range   = 635     ;}  // get read in range from 0 to 512  mV
     else if(config.pga == 3){max_range   = 1271    ;}  // get read in range from 0 to 1024 mV
@@ -90,37 +98,48 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
     //critical zone start
     critical_section_enter_blocking(&myLock);
     
-    //From section [10.1.7] we obtain the sequence of operation for master bus
+    //From data sheet of ADS1115 p[35] in section [10.1.7] we obtain the sequence of operation for master bus
     switch (event) {
     case I2C_SLAVE_RECEIVE: // master has written some data  
         // writes always start with the memory address
         add_ptr_reg = i2c_read_byte_raw(i2c); 
-        // save configration  into configration register 
+        // save configration into configration register 
         if(add_ptr_reg == 1){
-            uint8_t buffer[2];
+            uint8_t buffer[2];       //array to receive data from master 
+            //receving data from master MSB then LSB
             i2c_read_blocking(i2c, I2C_SLAVE_ADDRESS, buffer, 2, false);
             msb = buffer[0];
             lsb = buffer[1];
-            set_configration = (msb << 8) | lsb;
-            memcpy(&config, &set_configration, sizeof(uint16_t));
-
+            set_configration = (msb << 8) | lsb;                    //set bits in right position
+            memcpy(&config, &set_configration, sizeof(uint16_t));  //copy configration in bitfield
         }  
-        if(add_ptr_reg == 2){i2c_read_blocking(i2c, I2C_SLAVE_ADDRESS, &lo_thresh_reg,2, false);}  
-        if(add_ptr_reg == 3){i2c_read_blocking(i2c, I2C_SLAVE_ADDRESS, &ho_thresh_reg,2, false);}         
+        if(add_ptr_reg == 2){
+                i2c_read_blocking(i2c, I2C_SLAVE_ADDRESS, &lo_thresh_reg,2, false);
+            }  
+        if(add_ptr_reg == 3){
+                i2c_read_blocking(i2c, I2C_SLAVE_ADDRESS, &ho_thresh_reg,2, false);
+            }         
         break;
     case I2C_SLAVE_REQUEST: // master is requesting data load from ADC
+        //Sending data MSB then LSB
         if(add_ptr_reg == 1){
             i2c_write_byte_raw(i2c, (set_configration >> 8) );
             i2c_write_byte_raw(i2c, set_configration);
         }
-        else{
-        adc_settings();
-        get_adc_read();
-        printf("pin[%u]-pga[%u]\n",(config.mux %4),config.pga);
-        
-        i2c_write_byte_raw(i2c, (response >> 8) );
-        i2c_write_byte_raw(i2c, response);}
-        break;
+        else if (add_ptr_reg == 2) {
+            i2c_write_byte_raw(i2c, (lo_thresh_reg >> 8) );
+            i2c_write_byte_raw(i2c, lo_thresh_reg);
+        }
+        else if (add_ptr_reg == 3) {
+            i2c_write_byte_raw(i2c, (ho_thresh_reg >> 8) );
+            i2c_write_byte_raw(i2c, ho_thresh_reg);
+        }
+        else  {
+            adc_settings();
+            get_adc_read();        
+            i2c_write_byte_raw(i2c, (response >> 8) );
+            i2c_write_byte_raw(i2c, response);
+        }break;
     case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
         break;
     default:
@@ -160,5 +179,6 @@ int main() {
     //operating function 
     setup_slave();
     
-    while (true); 
+    while (true);
+    
 }
